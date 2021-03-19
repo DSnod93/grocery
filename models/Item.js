23de5af57338d50c913c743ceca30b5dddf08d00
Samuel <samuel@Samuels-MacBook-Pro.local>
2021-03-18 21:02:52 -0700

    added item model and corrected user model

---
+++ models/Item.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our Post model
+class Item extends Model {}
+
+// create fields/columns for Item model
+Item.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      image_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          isInt: true
+        }
+      },
+      price: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+        validate: {
+          isDecimal: true
+        }
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'Item'
+    }
+  );
+
+  module.exports = Item;
