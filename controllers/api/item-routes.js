f13fc2ffd6d6d816daf0ff40ca6d6a35c78ff400
Samuel <samuel@Samuels-MacBook-Pro.local>
2021-03-20 16:20:30 -0700

    added delete route endpoint

--- controllers/api/item-routes.js
+++ controllers/api/item-routes.js
@@ -16,22 +16,44 @@ router.get('/', (req,res) => {
 
 // get item by id (....../api/items/1)
 router.get ('/:id', (req,res) =>{
-Item.findOne({
-    where: {
-        id: req.params.id
-    }
-})
-.then(dbItemData => {
-    if (!dbItemData){
-        res.status(404).json({ message: 'No user found with this id' });
-          return;
-    }
+    Item.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbItemData => {
+        if (!dbItemData){
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
 
-    res.json(dbItemData);
-})
-.catch(err => {
-    console.log(err);
-    res.status(500).json(err);
+        res.json(dbItemData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+
 });
 
+router.delete('/:id', (req, res) => {
+    Item.destroy({
+     where: {
+         id: req.params.id
+     }
+    })
+    .then(dbItemData => {
+        if (!dbItemData){
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+
+        res.json(dbItemData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+
 });
+
