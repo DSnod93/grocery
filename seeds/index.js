cdb0143989370f7c6d2948f4bb9ad03bc927aeab
Samuel <samuel@Samuels-MacBook-Pro.local>
2021-03-21 19:51:56 -0700

    added user seeds

---
+++ seeds/index.js
@@ -0,0 +1,19 @@
+const seedUsers = require('./user-seeds');
+const seedItems = require('./item-seeds');
+
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('--------------');
+  await seedUsers();
+  console.log('--------------');
+
+  await seedItems();
+  console.log('--------------');
+
+  process.exit(0);
+};
+
+seedAll();
