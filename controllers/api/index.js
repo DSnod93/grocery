b50d1ee41fdd9d0c371c625c99fd3dab9e45b8ff
DSnoddy <douglassnoddyjr@gmail.com>
2021-03-22 11:56:52 -0800

    added items route to index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
+const userRoutes = require('./user-routes');
+const itemRoutes = require('./item-routes');
 
 
 router.use('/users', userRoutes);
+router.use('/items', itemRoutes);
 
 module.exports = router;
