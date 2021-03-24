2ffc3561a7ff57e485766831469c9d0587de4751
DSnoddy <douglassnoddyjr@gmail.com>
2021-03-23 16:08:11 -0800

    login setup

---
+++ public/javascript/login.js
@@ -0,0 +1,8 @@
+async function loginFormHanlder(event) {
+    event.preventDefault();
+
+const emailUser = document.querySelector('#first_name').value.trim();
+const password = document.querySelector('#password').value.trim();
+
+
+}
